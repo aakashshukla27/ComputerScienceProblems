4e09dd79bdb94b13480cd9efa3d697175e467338
Aakash Shukla <shukla.aa@northeastern.edu>
2021-03-12 15:06:55 -0500

    Add project files.

---
+++ Fibonacci/FibonacciHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class FibonacciHelper
+    {
+        /// <summary>
+        /// Simple calculation using recursion
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int fibonacci1(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+            return fibonacci1(n - 1) + fibonacci1(n - 2);
+        }
+
+
+        public Dictionary<int, int> memo = new Dictionary<int, int>();
+
+        /// <summary>
+        /// Searching Ditionary if record exists to control the number of times we recursively call the function
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int fibonacci2(int n)
+        {
+            if (!memo.ContainsKey(n))
+            {
+                memo.Add(n, fibonacci2(n - 1) + fibonacci2(n - 2));
+            }
+            return memo[n];
+        }
+
+        public void memoization()
+        {
+            memo.Add(0, 0);
+            memo.Add(1, 1);
+        }
+
+        public int fibonacci3(int n)
+        {
+            int last = 0, next = 1;  //fibonacci3(0) and fibonacci(1)
+            for (int i = 0; i < n; i++)
+            {
+                int oldLast = last;
+                last = next;
+                next = oldLast + next;
+            }
+            return last;
+        }
+
+
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonacciHelper fibonacci = new FibonacciHelper();
+
+            //Fibonacci 1 - using recursion
+            Stopwatch timer1 = new Stopwatch();
+            timer1.Start();
+            Console.WriteLine("Fibonacci 41 (method 1) = " + fibonacci.fibonacci1(40));
+            timer1.Stop();
+            Console.WriteLine("Time taken : " + timer1.Elapsed.ToString(@"m\:ss\.fff"));
+
+            //Fibonacci 2 - using dictionary
+            Stopwatch timer2 = new Stopwatch();
+            fibonacci.memoization();
+            timer2.Start();
+            Console.WriteLine("Fibonacci 41 (method 2) = " + fibonacci.fibonacci2(40));
+            timer2.Stop();
+            Console.WriteLine("Time taken : " + timer2.Elapsed.ToString(@"m\:ss\.fff"));
+
+            //Fibonacci 3 - using iteration
+            Stopwatch timer3 = new Stopwatch();
+            timer3.Start();
+            Console.WriteLine("Fibonacci 41 (method 3) = " + fibonacci.fibonacci2(40));
+            timer3.Stop();
+            Console.WriteLine("Time taken : " + timer3.Elapsed.ToString(@"m\:ss\.fff"));
+            Console.ReadKey();
+        }
+    }
+}
