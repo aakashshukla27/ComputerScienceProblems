e22a792b638e2f75748917da57d2326e302ec365
Aakash Shukla <shukla.aa@northeastern.edu>
2021-03-12 15:28:20 -0500

    Genome Compression problem added

---
+++ Compression/CompressedGene.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compression
+{
+    class CompressedGene
+    {
+        public BitArray bitArray;
+        private int length;
+
+        public CompressedGene(string gene)
+        {
+            compress(gene);
+        }
+
+        private void compress(string gene)
+        {
+            length = gene.Length;
+            bitArray = new BitArray(length * 2);
+            string upperGene = gene.ToUpper();
+            for(int i=0; i<length; i++)
+            {
+                int firstLocation = 2 * i;
+                int secondLocation = 2 * i + 1;
+                switch (upperGene[i])
+                {
+                    case 'A':
+                        bitArray.Set(firstLocation, false);
+                        bitArray.Set(secondLocation, false);
+                        break;
+                    case 'C':
+                        bitArray.Set(firstLocation, false);
+                        bitArray.Set(secondLocation, true);
+                        break;
+                    case 'G':
+                        bitArray.Set(firstLocation, true);
+                        bitArray.Set(secondLocation, false);
+                        break;
+                    case 'T':
+                        bitArray.Set(firstLocation, true);
+                        bitArray.Set(secondLocation, true);
+                        break;
+                    default:
+                        throw new Exception("Gene string contains characters other than ACGT.");
+                }
+            }
+        }
+
+
+        public String decompress()
+        {
+            if(bitArray == null)
+            {
+                return "";
+            }
+            StringBuilder stringBuilder = new StringBuilder(length);
+            for (int i = 0; i < (length * 2); i += 2)
+            {
+                int firstBit = (bitArray.Get(i) ? 1 : 0);
+                int secondBit = (bitArray.Get(i + 1) ? 1 : 0);
+                int lastBits = firstBit << 1 | secondBit;
+                switch (lastBits)
+                {
+                    case 0b00: // 00 is 'A'
+                        stringBuilder.Append('A');
+                        break;
+                    case 0b01: // 01 is 'C'
+                        stringBuilder.Append('C');
+                        break;
+                    case 0b10: // 10 is 'G'
+                        stringBuilder.Append('G');
+                        break;
+                    case 0b11: // 11 is 'T'
+                        stringBuilder.Append('T');
+                        break;
+                }
+            }
+
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Compression/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String original = "TAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATATAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATA";
+            CompressedGene compressed = new CompressedGene(original);
+            Console.WriteLine("Compressed Sequence: ");
+            foreach(bool bit in compressed.bitArray)
+            {
+                Console.Write(Convert.ToInt16(bit));
+            }
+            Console.WriteLine("\n");
+            string decompressed = compressed.decompress();
+            Console.WriteLine(decompressed);
+            Console.ReadKey();
+        }
+    }
+}
